5c07a03e84da3014564d9121576c0af4774e0287
Tyler Shambora <tyler@theshamboras.com>
2016-04-24 13:42:59 -0700

    wip

--- brianwilliams-v2.js
+++ brianwilliams-v2.js
@@ -127,14 +127,14 @@ controller.hears([/post to (\S+)\n([\s\S]*)/], 'direct_message', function(bot, m
 });
 
 controller.hears(['hello', 'hi', 'hey'], ['direct_message', 'mention', 'direct_mention'], function(bot, message) {
-  console.log('ding');
-  console.log(message);
-  var validateName = getRealNameFromId(bot, message.user);
-  console.log(validateName);
-  bot.reply(message, 'Hello!');
-  if (validateName) {
-    bot.reply(message, 'Hey! You\'re pretty valid!');
-  }
+  getRealNameFromId(bot, message.user)
+    .then(isValidUser)
+    .then(function(result) {
+      bot.reply(message, 'Hello!');
+      if (validateName) {
+        bot.reply(message, 'Hey! You\'re pretty valid!');
+      }
+    });
 });
 
 controller.on('direct_message, mention, direct_mention', function(bot, message) {
